f095a9904059f322ba15f9afcd4f068fd5f039c6
alasgar88 <85016138+alasgar88@users.noreply.github.com>
2022-09-19 16:52:27 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { newUser } from "./feautures/users/userSlice";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserInfo from "./containers/UserInfo/UserInfo";
-const socket = new WebSocket("ws://simple-realtime-server.herokuapp.com/");
+const socket = new WebSocket("wss://simple-realtime-server.herokuapp.com/");
 
 function App() {
   const { user } = useSelector((store) => store.user);
